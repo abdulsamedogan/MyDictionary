ab5c411ea7f53670d25704fa85df60af30c25963
Abdül Samed Doğan <samet.fb177@gmail.com>
2021-01-22 19:03:36 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(1, "Samed Dogan");
+            ogrenciler.Add(2, "Engin Demiroğ");
+
+           
+        }
+    }
+}
